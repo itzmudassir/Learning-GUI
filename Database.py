82485b599909f8a8a9363179d3d28439316b751b
itzmudasiir <itzmudassir07@gmail.com>
2022-07-19 17:11:30 +0500

    Customization!

--- Database.py
+++ Database.py
@@ -18,35 +18,35 @@ def query():
     return
 
 # Creating first Label
-Label(root, text="First Name", fg="black").grid(row=0, column=0,sticky=E)
+Label(root, text="First Name", fg="black", font="dejavu 10 bold").grid(row=0, column=0, sticky=W)
 # Creating first Entry
 text_entry = Entry(root, width=35, border=2)
 text_entry.grid(row=0, column=1,padx=10, pady=10)
 # Creating second Label
-Label(root, text="Last Name",fg="black").grid(row=1, column=0,sticky=E)
+Label(root, text="Last Name",fg="black", font="dejavu 10 bold").grid(row=1, column=0,sticky=W)
 # Creating second Entry
 text_entry1 = Entry(root, width=35, border=2)
 text_entry1.grid(row=1, column=1,padx=10, pady=10)
 # Creating third Label
-Label(root, text="City").grid(row=2, column=0,sticky=E)
+Label(root, text="City", font="dejavu 10 bold").grid(row=2, column=0,sticky=W)
 # Creating third Entry
 text_entry2 = Entry(root, width=35, border=2)
 text_entry2.grid(row=2, column=1,padx=10, pady=10)
 # Creating fourth Label
-Label(root, text="Address").grid(row=3, column=0,sticky=E)
+Label(root, text="Address", font="dejavu 10 bold").grid(row=3, column=0,sticky=W)
 # Creating fourth Entry
 text_entry3 = Entry(root, width=35, border=2)
 text_entry3.grid(row=3, column=1,padx=10, pady=10)
 # Creating fifth Label
-Label(root, text="Zip code").grid(row=4, column=0,sticky=E)
+Label(root, text="Zip code", font="dejavu 10 bold").grid(row=4, column=0,sticky=W)
 # Creating fifth Entry
 text_entry4 = Entry(root, width=35, border=2)
 text_entry4.grid(row=4, column=1,padx=10, pady=10)
 
 # Creating a button
-Button(root, text="Submit",padx=5, pady=5, command=submit).grid(row=5, column=1)  # Button to submit
-Button(root, text="Query",padx=5, pady=5, command=query).grid(row=5, column=0)    # Button for query
-Button(root, text="Exit",padx=20, pady=5, command=root.destroy).grid(row=5, column=2)  # Button to exit
+Button(root, text="Submit",padx=5, pady=5, bg="#0DF017", fg="#565446", command=submit).grid(row=5, column=1)  # Button to submit
+Button(root, text="Query",padx=5, pady=5,bg="#565446", fg="white", command=query).grid(row=5, column=0)    # Button for query
+Button(root, text="Exit",padx=20, pady=5,bg="#F00D4E", command=root.destroy).grid(row=5, column=2)  # Button to exit
 
 
 
