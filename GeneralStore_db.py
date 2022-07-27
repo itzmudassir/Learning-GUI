7524f2ebf797548df6c9416bedbd38987e0e05f1
itzmudasiir <itzmudassir07@gmail.com>
2022-07-27 19:09:49 +0500

    Added entries and labels to store data in db

--- GeneralStore_db.py
+++ GeneralStore_db.py
@@ -23,5 +23,45 @@ cursor1 = database.cursor()
 
 
 
+# Creating Entries and Labels *********
+
+# Creating first Label
+Label(root, text="First Name", fg="black", font="dejavu 10 bold").grid(row=2, column=0, sticky=W)
+# Creating first Entry
+text_entry = Entry(root, width=35, border=2)
+text_entry.grid(row=2, column=1,padx=10, pady=10)
+# Creating second Label
+Label(root, text="Last Name",fg="black", font="dejavu 10 bold").grid(row=3, column=0,sticky=W)
+# Creating second Entry
+text_entry1 = Entry(root, width=35, border=2)
+text_entry1.grid(row=3, column=1,padx=10, pady=10)
+# Creating third Label
+Label(root, text="Phone Number", font="dejavu 10 bold").grid(row=4, column=0,sticky=W)
+# Creating third Entry
+text_entry2 = Entry(root, width=35, border=2)
+text_entry2.grid(row=4, column=1,padx=10, pady=10)
+# Creating fourth Label
+Label(root, text="Address", font="dejavu 10 bold").grid(row=5, column=0,sticky=W)
+# Creating fourth Entry
+text_entry3 = Entry(root, width=35, border=2)
+text_entry3.grid(row=5, column=1,padx=10, pady=10)
+# Creating fifth Label
+Label(root, text="City", font="dejavu 10 bold").grid(row=6, column=0,sticky=W)
+# Creating fifth Entry
+text_entry4 = Entry(root, width=35, border=2)
+text_entry4.grid(row=6, column=1,padx=10, pady=10)
+# Creating sixth Label
+Label(root, text="Zip Code", font="dejavu 10 bold").grid(row=7, column=0,sticky=W)
+# Creating sixth Entry
+text_entry5 = Entry(root, width=35, border=2)
+text_entry5.grid(row=7, column=1,padx=10, pady=10)
+# Creating Search Label
+Label(root, text="Search", font="dejavu 10 bold").grid(row=0, column=0,sticky=W)
+# Creating search entry
+search_entry = Entry(root, width=35, border=2)
+search_entry.grid(row=0, column=1,padx=10, pady=10)
+
+
+
 
 root.mainloop()
