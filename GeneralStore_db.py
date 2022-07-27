2aa4b957082e9a49192b844f240c9d08d9f524c1
itzmudasiir <itzmudassir07@gmail.com>
2022-07-27 19:15:05 +0500

    Added submit function

--- GeneralStore_db.py
+++ GeneralStore_db.py
@@ -3,7 +3,7 @@ import mysql.connector
 
 root = Tk()
 root.title("General Store")
-root.wm_attributes('-fullscreen', 'false')    # Screen Size
+root.wm_attributes('-fullscreen', 'True')    # Screen Size
 root.configure(background='white')    # Background color of Window
 
 # Creating database connection
@@ -20,6 +20,31 @@ cursor1 = database.cursor()
 #cursor1.execute("CREATE TABLE General(frist_name VARCHAR(255),last_name VARCHAR(255),age INT(255),zipcode INT(10),price_paid DECIMAL(10,2),user_id INT AUTO_INCREMENT PRIMARY KEY)")
 #cursor1.execute("SELECT * FROM General")
 #print(cursor1.description)
+
+
+
+# Creating Functions *****
+
+# Creating Submit function
+def submit():
+     # Deleting the first text entry
+    text_entry.delete(0, END)
+    # Deleting the second text entry
+    text_entry1.delete(0, END)
+    # Deleting the third text entry
+    text_entry2.delete(0, END)
+    # Deleting the fourth text entry
+    text_entry3.delete(0, END)
+    # Deleting the fifth text entry
+    text_entry4.delete(0, END)
+    # Deleting the sixth text entry
+    text_entry5.delete(0, END)
+
+
+def query():
+    return
+
+
 
 
 
@@ -60,6 +85,13 @@ Label(root, text="Search", font="dejavu 10 bold").grid(row=0, column=0,sticky=W)
 # Creating search entry
 search_entry = Entry(root, width=35, border=2)
 search_entry.grid(row=0, column=1,padx=10, pady=10)
+
+
+
+# Creating buttons *********
+Button(root, text="Submit",padx=5, pady=5, command=submit).grid(row=8, column=1)  # Button to submit
+Button(root, text="Show Record",padx=5, pady=5,bg="#565446", fg="white", command=query).grid(row=8, column=0)    # Button for query
+Button(root, text="X",padx=10, bg="#F00D4E", command=root.destroy).place(x=1330, y=0)  # Button to exit
 
 
 
