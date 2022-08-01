5841a8ed18d87f7c5403815d5a20050ecf476920
itzmudasiir <itzmudassir07@gmail.com>
2022-08-01 19:19:28 +0500

    Adding functions

--- GeneralStore_db.py
+++ GeneralStore_db.py
@@ -1,9 +1,7 @@
 # PROGRAMMED BY: Mudassir Nadeem
 
 from tkinter import *
-from turtle import bgcolor
 import mysql.connector
-from PIL import ImageTk,Image
 
 root = Tk()
 root.title("General Store")
@@ -27,12 +25,29 @@ cursor1 = database.cursor()
 #cursor1.execute("SELECT * FROM General")
 #print(cursor1.description)
 
+# Adding "FIELDS to the database"
+'''cursor1.execute("ALTER TABLE General ADD  (\
+    email VARCHAR(255),\
+    address_1 VARCHAR(255),\
+    address_2 VARCHAR(255),\
+    city VARCHAR(50),\
+    state VARCHAR(50),\
+    country VARCHAR(255),\
+    phone VARCHAR(255),\
+    payment_method VARCHAR(50),\
+    discount_code VARCHAR(255))")  
+'''
+
+#cursor1.execute("SELECT * FROM General")
+#result=cursor1.description
+#for x in result:
+   #print(x)
 
 
 # ***FUNCTIONS START HERE***
 
 # Creating Submit function
-def submit():
+def clear_fields():
      # Deleting the first text entry
     text_entry.delete(0, END)
     # Deleting the second text entry
@@ -54,9 +69,6 @@ def submit():
     # Deleting the tenth text entry
     text_entry9.delete(0, END)
 
-
-#def query():
-    #return
 
 
 # Creating FUNCTIONS to remove text from the text entry
@@ -80,8 +92,12 @@ def temp_text8(e):
     text_entry8.delete(0,"end")
 def temp_text9(e):
     text_entry9.delete(0,"end")
-   
+
+# Creating function to insert data into the database
+def add_customer():
     
+	sql_command = "INSERT INTO General(frist_name,last_name,phone,email,address_1,city,state,zipcode,price_paid,user_id) VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+    values = (text_entry.get(),text_entry1.get(),text_entry2.get(),text_entry3.get(),text_entry4.get(),text_entry5.get(),text_entry6.get(),text_entry7.get(),text_entry8.get(),text_entry9.get())
 
 
 # Creating a "Title" Label
@@ -91,13 +107,15 @@ title_e = Label(root, text=" Store", font=("Helvetica", 40), bg="black", fg="whi
 # Creating "First Name" Label
 first_name = Label(root, text="First Name", font=("Helvetica", "12", "bold"), bg="white").place(x=5, y=100)
 # Creating a "First Name" Text Entry
+global text_entry
 text_entry = Entry(root, width=30, bg="white", border = 2)
-text_entry.insert(0, "Jack", foreground="white")
+text_entry.insert(0, "Jack")
 text_entry.place(x=180, y=102)
 text_entry.bind("<FocusIn>", temp_text)
 # Creating "Last Name" Label
 last_name = Label(root, text="Last Name", font=("Helvetica", "12", "bold"), bg="white").place(x=5, y=150)
 # Creating a "Last Name" Text Entry
+global text_entry1
 text_entry1 = Entry(root, width=30, bg="white", border = 2)
 text_entry1.insert(0, "Smith")
 text_entry1.bind("<FocusIn>", temp_text1)
@@ -105,6 +123,7 @@ text_entry1.place(x=180, y=152)
 # Creating "Phone Number" Label
 phone_number = Label(root, text="Phone Number", font=("Helvetica", "12", "bold"), bg="white").place(x=5, y=200)
 # Creating a "Phone Number" Text Entry
+global text_entry2
 text_entry2 = Entry(root, width=30, bg="white", border = 2)
 text_entry2.insert(0, "1234567890")
 text_entry2.bind("<FocusIn>", temp_text2)
@@ -112,6 +131,7 @@ text_entry2.place(x=180, y=202)
 # Creating "Email" Label
 email = Label(root, text="Email", font=("Helvetica", "12", "bold"), bg="white").place(x=5, y=250)
 # Creating a "Email" Text Entry
+global text_entry3
 text_entry3 = Entry(root, width=30, bg="white", border = 2)
 text_entry3.insert(0, "@example.com")
 text_entry3.bind("<FocusIn>", temp_text3)
@@ -119,6 +139,7 @@ text_entry3.place(x=180, y=252)
 # Creating "Address" Label
 address = Label(root, text="Address", font=("Helvetica", "12", "bold"), bg="white").place(x=5, y=300)
 # Creating a "Address" Text Entry
+global text_entry4
 text_entry4 = Entry(root, width=30, bg="white", border = 2)
 text_entry4.insert(0, "123 Main St")
 text_entry4.bind("<FocusIn>", temp_text4)
@@ -126,6 +147,7 @@ text_entry4.place(x=180, y=302)
 # Creating "City" Label
 city = Label(root, text="City", font=("Helvetica", "12", "bold"), bg="white").place(x=5, y=350)
 # Creating a "City" Text Entry
+global text_entry5
 text_entry5 = Entry(root, width=30, bg="white", border = 2)
 text_entry5.insert(0, "New York")
 text_entry5.bind("<FocusIn>", temp_text5)
@@ -133,13 +155,15 @@ text_entry5.place(x=180, y=352)
 # Creating "State" Label
 state = Label(root, text="State", font=("Helvetica", "12", "bold"), bg="white").place(x=5, y=400)
 # Creating a "State" Text Entry
+global text_entry6
 text_entry6 = Entry(root, width=30, bg="white", border = 2)
 text_entry6.insert(0, "NY")
 text_entry6.bind("<FocusIn>", temp_text6)
 text_entry6.place(x=180, y=402)
 # Creating "Zip Code" Label
-zip_code = Label(root, text="Zip Code", font=("Helvetica", "12", "bold"), bg="white").place(x=5, y=450)
+zip_code = Label(root, text="Zi Code", font=("Helvetica", "12", "bold"), bg="white").place(x=5, y=450)
 # Creating a "Zip Code" Text Entry
+global text_entry7
 text_entry7 = Entry(root, width=30, bg="white", border = 2)
 text_entry7.insert(0, "10001")
 text_entry7.bind("<FocusIn>", temp_text7)
@@ -147,6 +171,7 @@ text_entry7.place(x=180, y=452)
 # Creating "Price Paid" Label
 price_paid = Label(root, text="Price Paid", font=("Helvetica", "12", "bold"), bg="white").place(x=5, y=500)
 # Creating a "Price Paid" Text Entry
+global text_entry8
 text_entry8 = Entry(root, width=30, bg="white", border = 2)
 text_entry8.insert(0, "100.00")
 text_entry8.bind("<FocusIn>", temp_text8)
@@ -154,19 +179,17 @@ text_entry8.place(x=180, y=502)
 # Creating "User ID" Label
 user_id = Label(root, text="User ID", font=("Helvetica", "12", "bold"), bg="white").place(x=5, y=550)
 # Creating a "User ID" Text Entry
+global text_entry9
 text_entry9 = Entry(root, width=30, bg="white", border = 2)
 text_entry9.insert(0, "1")
 text_entry9.bind("<FocusIn>", temp_text9)
 text_entry9.place(x=180, y=552)
 # Creating "Submit" Button
-submit = Button(root, text="Submit", font=("Helvetica", "12", "bold"), bg="black", fg="white", command=submit).place(x=100, y=600)
+submit = Button(root, text="Clear", font=("Helvetica", "12", "bold"), bg="black", fg="white", command=clear_fields).place(x=100, y=600)
 
 
 
 # CREATING BUTTONS
-
-#Button(root, text="Submit",padx=5, pady=5).grid(row=8, column=1)  # Button to submit
-#Button(root, text="Show Record",padx=5, pady=5,bg="#565446", fg="white").grid(row=8, column=0)    # Button for query
 Button(root, text="X",padx=10, bg="#F00D4E", command=root.destroy).place(x=1330, y=0)  # Button to exit
 
 
